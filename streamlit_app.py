f7432c5cabeecb5f22b17c9f80ac53e394a4256d
veronikavaraksina <83449904+veronikavaraksina@users.noreply.github.com>
2023-08-07 09:21:58 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,14 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subheader('Breakfast menu')
+streamlit.subheader('Breakfast Favourites')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avicado Toast')
+
+streamlit.subheader('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
