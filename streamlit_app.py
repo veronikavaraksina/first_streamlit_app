e9ac944b571ba820b498ecf5e5c1c16a74748e00
veronikavaraksina <83449904+veronikavaraksina@users.noreply.github.com>
2023-08-07 14:38:41 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
+import urllib.error import URLError
+
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -39,7 +42,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # puts data into a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
